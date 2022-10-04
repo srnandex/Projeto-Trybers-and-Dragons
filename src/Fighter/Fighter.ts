a51701360fb1335bb282b61ab8a59b69c1fa8cf6
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-10-04 03:02:48 -0300

    requisitos 6 e 8 finalizados

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,12 @@
+interface Fighter {
+  lifePoints: number,
+  strength: number,
+  defense: number,
+
+  attack(enemy: Fighter): void;
+  special(enemy: Fighter): void;
+  levelUp(): void;
+  receiveDamage(attackPoints: number): number;
+}
+
+export default Fighter;
